4548585d4fcc1517040a7f8e880ef4a78cc77d9c
Andreas T Jonsson <mail@andreasjonsson.se>
2024-11-11 13:22:19 +0100

    Added extra files

---
+++ tools/package/itch/README.rasberrypi.txt
@@ -0,0 +1,1 @@
+Copy all files in the RasberryPi{model} directory to the root directory of a FAT32 formated SDCard.
--- tools/package/itch/package.rasberrypi.sh
+++ tools/package/itch/package.rasberrypi.sh
@@ -2,12 +2,17 @@
 
 DIR=${GITHUB_WORKSPACE}/package
 rm -rf ${DIR}
-mkdir ${DIR}
 
-cp ${CIRCLEHOME}/boot/*.dtb ${DIR}
-cp ${CIRCLEHOME}/boot/*.dat ${DIR}
-cp ${CIRCLEHOME}/boot/*.bin ${DIR}
-cp ${CIRCLEHOME}/boot/*.elf ${DIR}
+DIR_RPI3=${DIR}/RaspberryPi3
+mkdir -p ${DIR_RPI3}
 
-cp ../../../front/rpi/kernel8-32.img ${DIR}
-cp ${CIRCLEHOME}/boot/config32.txt ${DIR}/config.txt
+cp ${CIRCLEHOME}/boot/*.dtb ${DIR_RPI3}
+cp ${CIRCLEHOME}/boot/*.dat ${DIR_RPI3}
+cp ${CIRCLEHOME}/boot/*.bin ${DIR_RPI3}
+cp ${CIRCLEHOME}/boot/*.elf ${DIR_RPI3}
+
+cp ../../../front/rpi/kernel8-32.img ${DIR_RPI3}
+cp ${CIRCLEHOME}/boot/config32.txt ${DIR_RPI3}/config.txt
+cp boot/freedos_web_hd.img ${DIR_RPI3}/C.img
+
+cp tools/package/itch/README.rasberrypi.txt ${DIR}/README.txt
