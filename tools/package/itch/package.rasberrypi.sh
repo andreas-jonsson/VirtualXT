82ba915824c568f483c905f2275baf7e0f38c9c8
Andreas T Jonsson <mail@andreasjonsson.se>
2024-11-11 13:45:21 +0100

    Added missing files to package

--- tools/package/itch/package.rasberrypi.sh
+++ tools/package/itch/package.rasberrypi.sh
@@ -11,8 +11,11 @@ cp ${CIRCLEHOME}/boot/*.dat ${DIR_RPI3}
 cp ${CIRCLEHOME}/boot/*.bin ${DIR_RPI3}
 cp ${CIRCLEHOME}/boot/*.elf ${DIR_RPI3}
 
-cp ../../../front/rpi/kernel8-32.img ${DIR_RPI3}
+cp front/rpi/kernel8-32.img ${DIR_RPI3}
 cp ${CIRCLEHOME}/boot/config32.txt ${DIR_RPI3}/config.txt
+
 cp boot/freedos_web_hd.img ${DIR_RPI3}/C.img
+cp bios/GLABIOS.ROM ${DIR_RPI3}
+cp bios/vxtx.bin ${DIR_RPI3}
 
 cp tools/package/itch/README.rasberrypi.txt ${DIR}/README.txt
