d2e94881fa323338357b2e44d4111b6035e9533d
andreas-jonsson <>
2024-11-13 21:04:47 +0000

    Deploying to gh-pages from @ andreas-jonsson/virtualxt@8668e3938064d955854f9930c8590eb31cac9b00 🚀

--- docs/structvxtu__disk__interface2.js
+++ docs/structvxtu__disk__interface2.js
@@ -1,6 +1,6 @@
 var structvxtu__disk__interface2 =
 [
     [ "num_sectors", "structvxtu__disk__interface2.html#a4342fc49e66c8191da17d1ab476455a2", null ],
-    [ "read_sector", "structvxtu__disk__interface2.html#a835501fe406ae30ee30cdd53467abc6d", null ],
-    [ "write_sector", "structvxtu__disk__interface2.html#a6636ef1f877d685b9cf18bd06f881785", null ]
+    [ "read_sector", "structvxtu__disk__interface2.html#a96013114ec11fa738bc1b428f9138a37", null ],
+    [ "write_sector", "structvxtu__disk__interface2.html#a807e322dbdd35e96822a4205cba94c5d", null ]
 ];
